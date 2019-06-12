653eaa8a270d956859f55baaaef612468a2d95cd
Renato Fabbri <renato.fabbri@gmail.com>
2019-06-12 18:33:57 -0300

    adiantado artigo, backup

---
+++ doc/mkFigBr.py
@@ -0,0 +1,47 @@
+import pylab as p, numpy as n
+
+fig=p.figure(figsize=(6.,4.))
+p.xlim(0,1)
+p.ylim(0,1)
+
+xmargin = 0.1
+amp = 0.7
+offset = 0.1
+x = n.linspace(0+xmargin, 0.5, 1000)
+y = 0.5 + 0.5 * amp * n.sin(n.linspace(0, n.pi, 1000)) - offset
+p.plot(x, y, label='hubs')
+
+p.annotate('', (1-xmargin, 0.2), (1, 0.2), ha="center", va="center", arrowprops=dict(arrowstyle="|-|"))
+p.annotate(r'$\mu$', (0.94, 0.22), fontsize=14)
+
+# p.arrow(0.1, 0.7, 0.1, 0.2, arrowprops=dict(arrowstyle="|-|"))
+p.annotate('', (0.05, y.max()), (0.05, y.min()), ha="center", va="center", arrowprops=dict(arrowstyle="|-|"))
+p.annotate(r'$\alpha$', (0.07, y.max()*0.75), fontsize=14)
+
+p.annotate('', (0.2, 0.5), (0.2, y.min()), ha="center", va="center", arrowprops=dict(arrowstyle="|-|"))
+p.annotate(r'$\Delta$', (0.22, 0.43), fontsize=14)
+
+x = n.linspace(0.5, 1-xmargin, 1000)
+y = 0.5 + 0.5 * amp * n.sin(n.linspace(n.pi, 2*n.pi, 1000)) - offset
+p.plot(x, y, label='intermediarios')
+
+x0, y0 = 0.8, 0.6
+x1, y1 = 0.4, 0.9
+
+xx = n.linspace(x0, x1, 100)
+yy = n.linspace(y0, y1, 100)
+ax = p.plot(xx, yy, label='periféricos')
+
+p.annotate(r'$(x_0, y_0)$', (x0, y0+.02), fontsize=14)
+p.annotate(r'$(x_1, y_1)$', (x1-0.02, y1+0.02), fontsize=14)
+
+p.xticks([0, 0.5, 1])
+p.yticks([0, 0.5, 1])
+p.legend()
+
+p.title("Posicionamento dos vértices no Versinus", fontsize=16)
+
+p.subplots_adjust(left=0.06, bottom=0.06, right=0.97, top=0.93)
+
+p.savefig("./nodePositioningBr.png")
+# p.show()
